61e076dffb16439eb62d9b46d4f290b9fb136710
Akif Feyzioğlu <orginalprof@gmail.com>
2020-09-18 10:18:28 +0300

    change for loop

--- Examples/exp.go
+++ Examples/exp.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	for x := 100; x >= 0; x= x - 5 {
+	for x := 100; x >= 0; x -= 10 {
 		fmt.Println(x)
 	} 
 }
 
-// 100'den 0'a kadar 5'er 5'er ekrana yazar. 
+// 100'den 0'a kadar 10'er 10'er ekrana yazar. 
